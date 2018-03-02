4fceb24578b5176273b8b33235353086b6e7df96
naveen semwal <naveenrhtdm4ever@gmail.com>
2018-03-02 20:22:55 +0530

    Model based form with Validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
 
+
+//  This is required for Model Based Form
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+
+  userForm = new FormGroup({
+
+    name: new FormControl(null,
+      [Validators.required, Validators.minLength(4), Validators.maxLength(6)]),
+
+
+    email: new FormControl(),
+    address: new FormGroup({
+
+      street: new FormControl(),
+      city: new FormControl(),
+      postalcode: new FormControl()
+
+    }),
+
+
+  })
+
+
+  SaveUserDetail() {
+
+    console.log(this.userForm.value);
+
+  }
+
+
+
+
 }
